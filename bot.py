b8d629003090c0f489b896de2a4067d2fcdfaf67
vlburmistov <s24b_burmistrov@179.ru>
2023-05-18 20:47:04 +0300

    изменение названия хендлера

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
-from handlers import other_handlers, user_handlers, Handlers
+from handlers import other_handlers, ___user_handlers, Handlers
 
 logger = logging.getLogger(__name__)
 
