5dfda2e27da179be80fb991988a93240c2510238
vlburmistov <s24b_burmistrov@179.ru>
2023-05-18 20:45:18 +0300

    "база" данных в виде python словаря

---
+++ database/database.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+users_db: dict = {}
+user_dict_template: dict = {'age': 0,
+                            'name': "name",
+                            'mail': "example@email.ru"}
