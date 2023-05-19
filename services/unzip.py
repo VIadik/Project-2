44b84914f5bd22a618efeb9f851007705d905a3d
vlburmistov <s24b_burmistrov@179.ru>
2023-05-20 00:53:06 +0300

    работающая реализация разархивации

--- services/unzip.py
+++ services/unzip.py
@@ -1,4 +1,12 @@
+import os
 import zipfile
 
-with zipfile.ZipFile("data/archive.zip", 'r') as zip_ref:
-    zip_ref.extractall("data/result")
+
+def main(user_id: int):
+    files = os.listdir(f"users_files/{user_id}/documents")
+    if 'Icon\r' in files:
+        files.remove('Icon\r')
+    assert len(files) == 1
+    file = files[0]
+    with zipfile.ZipFile(f"users_files/{user_id}/documents/{file}", 'r') as zip_ref:
+        zip_ref.extractall(f"users_files/{user_id}/unzip_files")
