e86f29cd933a79a8eca4a763a61baa37bdff6884
vlburmistov <s24b_burmistrov@179.ru>
2023-05-20 12:57:31 +0300

    настроен порядок изображений

--- services/pdf.py
+++ services/pdf.py
@@ -5,9 +5,10 @@ import os
 def main(user_id: int):
     files = os.listdir(f"users_files/{user_id}/photos")
     os.chdir(f"users_files/{user_id}/photos")
-    sorted(filter(os.path.isfile, os.listdir('.')), key=os.path.getmtime)
     if 'Icon\r' in files:
         files.remove('Icon\r')
+    files.sort(key=os.path.getmtime)
+
     images = [Image.open(f"{file}") for file in files]
     pdf_path = f"../result.pdf"
     images[0].save(pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:])
